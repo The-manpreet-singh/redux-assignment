34394b8a83d456130ce885234c032a459fa909ef
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-21 12:07:59 +0530

    add ACTIONS inside reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,11 +1,21 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     persons: []
 };
    
 const reducer= (state= initialState, action) => {
     switch (action.type) {
+       case actionTypes.ADD_PERSON:
+           return {
 
+           }
+        case actionTypes.REMOVE_PERSON:
+            return {
+
+            }
     }
+   return state; 
 };
 
 export default reducer;
